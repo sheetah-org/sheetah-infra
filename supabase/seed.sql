c7c24984fd3c9b57562e42e2b53928442514d9e6
Iliyan Kupenov <i.kupenov96@gmail.com>
2024-03-07 15:46:21 +0200

    feat(supabase): revoke priviliges from anon by default

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,32 +1,74 @@
+-- Disable RLS for 'anon' role
+-- https://github.com/orgs/supabase/discussions/4547
+ALTER DEFAULT PRIVILEGES 
+IN SCHEMA public 
+REVOKE ALL ON TABLES 
+FROM anon;
+
+ALTER DEFAULT PRIVILEGES 
+IN SCHEMA public 
+REVOKE ALL ON FUNCTIONS 
+FROM anon;
+
+ALTER DEFAULT PRIVILEGES 
+IN SCHEMA public 
+REVOKE ALL ON SEQUENCES 
+FROM anon;
+
+REVOKE SELECT 
+ON ALL TABLES 
+IN SCHEMA public 
+FROM anon;
+
+REVOKE USAGE 
+ON ALL SEQUENCES 
+IN SCHEMA public 
+FROM anon;
+
+REVOKE EXECUTE 
+ON ALL FUNCTIONS 
+IN SCHEMA public 
+FROM anon;
+
+-- Create function that retrieves the JWT claims from the JWT token (used in Storage RLS)
 CREATE OR REPLACE FUNCTION requesting_user_id()
 RETURNS TEXT 
-LANGUAGE SQL stable
+LANGUAGE SQL STABLE
 AS $$
-  SELECT nullif(current_setting('request.jwt.claims', true)::json->>'sub', '')::text;
+SELECT nullif(current_setting('request.jwt.claims', true)::json->>'sub', '')::text;
 $$;
 
+-- Seed 'vault' bucket with RLS policies
 INSERT INTO storage.buckets(id, name, public) VALUES ('vault', 'vault', TRUE);
 
 CREATE POLICY "Give users access to own folder - SELECT"
-ON storage.objects FOR SELECT USING (
+ON storage.objects 
+FOR SELECT 
+USING (
     bucket_id = 'vault'
     AND requesting_user_id()::text = (storage.foldername(name))[1]
 );
 
 CREATE POLICY "Give users access to own folder - INSERT"
-ON storage.objects FOR INSERT WITH CHECK (
+ON storage.objects 
+FOR INSERT 
+WITH CHECK (
     bucket_id = 'vault'
     AND requesting_user_id()::text = (storage.foldername(name))[1]
 );
 
 CREATE POLICY "Give users access to own folder - UPDATE"
-ON storage.objects FOR UPDATE USING (
+ON storage.objects 
+FOR UPDATE 
+USING (
     bucket_id = 'vault'
     AND requesting_user_id()::text = (storage.foldername(name))[1]
 );
 
 CREATE POLICY "Give users access to own folder - DELETE"
-ON storage.objects FOR DELETE USING (
+ON storage.objects 
+FOR DELETE 
+USING (
     bucket_id = 'vault'
     AND requesting_user_id()::text = (storage.foldername(name))[1]
 );
